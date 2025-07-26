90dbf9bca8b802d839d1d38be8c6a28d49544b2b
Kohei Nishikawa <kn.linz@gmail.com>
2025-07-26 23:20:13 +0900

    外部ブラウザで開く機能の追加対応

--- preload.js
+++ preload.js
@@ -3,3 +3,8 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('keyAPI', {
   pressKeys: (keys, duration) => ipcRenderer.invoke('press-keys', keys, duration)
 });
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  openExternal: (url) => ipcRenderer.invoke("open-external", url),
+  saveURLConfig: (url) => ipcRenderer.invoke("save-url", url),
+});
